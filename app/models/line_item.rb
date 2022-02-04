60571196e14b6b6d0ae406165dc079fc89ccb464
James <jameshuang337@gmail.com>
2022-02-03 18:51:35 -0800

    add comment

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ActiveRecord::Base
 
   belongs_to :order
-  belongs_to :product
+  belongs_to :product # connects the line_item to that specific product; this allows the line_item to access product information as well
 
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
