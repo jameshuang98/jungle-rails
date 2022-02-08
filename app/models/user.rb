0de6f67bc82e8bb5bd95e09bd11a38ff2af3eb1d
James <jameshuang337@gmail.com>
2022-02-08 15:13:40 -0800

    create class method authenticate_with_credentials in User model and apply this method in sessions controller, also test that this method works as intended

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    validates :name, presence: true
+    # validates :last_name, presence: true
+    validates :password, presence: true, length: { minimum: 7 }
+    validates :password_confirmation, presence: true, length: { minimum: 7 }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+    before_save :downcase_email
+
+    def self.authenticate_with_credentials(email, password)
+        # make email all lowercase and remove any surrounding whitespace
+        clean_email = email.downcase.strip
+
+        # find the user with the find_by_email dynamic finder method
+        user = User.find_by_email(clean_email)
+        
+        # returns user if the user exists and passes the authentication
+        user && user.authenticate(password)
+    end
+
+    def downcase_email
+        self.email.downcase!
+    end
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,150 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    
+    before(:each) do
+      @user = User.new(
+        name: "username",
+        email: "test@test.com",
+        password: "password",
+        password_confirmation: "password"
+        )
+      # save the user to the database (this is necessary because I am checking for duplicate user credentials) 
+      @user.save 
+    end
+    
+    context "Create an initial example to ensure user will save successfully" do
+      it "it is valid with valid attributes" do
+        expect(@user).to be_valid
+      end
+    end
+
+    context "Check password and password_confirmation validations" do
+      it "it is not valid if password doesn't match password confirmation" do
+        @user.password = "laber"
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "it is not valid if password is blank" do
+        @user.password = nil
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it "it is not valid if password confirmation is blank" do
+        @user.password_confirmation = nil
+        expect(@user).to_not be_valid
+        puts @user.errors.full_messages
+        expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+      end
+    end
+
+    context "Check that emails must be unique" do
+      it "it is not valid if email already exists in database" do
+        
+        user2 = User.new(
+          name: "username2",
+          email: "test@test.com",
+          password: "password",
+          password_confirmation: "password"
+        )
+
+        expect(user2).to_not be_valid # This expect triggers the validations
+        expect(user2.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it "it is not valid if email (with different casing) already exists in database" do
+        
+        user3 = User.new(
+          name: "username2",
+          email: "TEST@TEST.com",
+          password: "password",
+          password_confirmation: "password"
+        )
+
+        expect(user3).to_not be_valid
+        expect(user3.errors.full_messages).to include("Email has already been taken")
+      end
+    end
+
+
+    context "Check that email, first name, and last name are not blank" do
+      
+      it "it is not valid without a name" do
+        @user.name = nil
+        expect(@user).to_not be_valid 
+        expect(@user.errors.full_messages).to include("Name can't be blank") 
+      end
+
+      # it "it is not valid without a last name" do
+      #   @user.last_name = nil
+      #   expect(@user).to_not be_valid 
+      #   expect(@user.errors.full_messages).to include("Last name can't be blank") 
+      # end
+
+      it "it is not valid without a email" do
+        @user.email = nil
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+    end
+
+    context "Check that password must be at least 7 characters" do
+      
+      it "it is not valid with a short password" do
+        @user.password = "pass"
+        @user.password_confirmation = "pass"
+        expect(@user).to_not be_valid
+        puts @user.errors.full_messages
+        expect(@user.errors.full_messages).to include("Password confirmation is too short (minimum is 7 characters)")
+      end
+
+    end
+
+    describe '.authenticate_with_credentials' do
+
+      before(:each) do
+        @person = User.create(
+          name: "person",
+          email: "RSpec@test.com",
+          password: "password",
+          password_confirmation: "password"
+          )
+      end
+    
+      context "Check that .authenticate_with_credentials works" do
+      
+        it "it works with valid credentials" do
+          expect(User.authenticate_with_credentials("RSpec@test.com", "password")).to eq(@person)
+        end
+
+        it "it doesn't with invalid email" do
+          expect(User.authenticate_with_credentials("RSpec@test.test.com", "password")).to_not eq(@person)
+        end
+
+        it "it doesn't with invalid password" do
+          expect(User.authenticate_with_credentials("RSpec@test.com", "passwordword")).to_not eq(@person)
+        end
+
+        it "it still works if there are leading spaces" do
+          expect(User.authenticate_with_credentials("   RSpec@test.com", "password")).to eq(@person)
+        end
+
+        it "it still works if there are trailing spaces" do
+          expect(User.authenticate_with_credentials("RSpec@test.com   ", "password")).to eq(@person)
+        end
+
+        it "it still works if email is typed with different casing" do
+          expect(User.authenticate_with_credentials("rsPec@TEST.com", "password")).to eq(@person)
+        end
+
+      
+      end
+      
+    end
+
+  end
+end
