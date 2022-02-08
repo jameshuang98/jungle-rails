f8606e315c4b77bfbc8e75790bb32792303b18e7
James <jameshuang337@gmail.com>
2022-02-07 18:41:20 -0800

    added comment about Money-Rails

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
-
+  # Using the Money-Rails gem, each Product object will have an attribute called price which is a Money object
   monetize :price_cents, numericality: true
+  
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
