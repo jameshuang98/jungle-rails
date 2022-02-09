afcc91821e5b9b209417e1ba1d667bd0b8fa5fd7
James <jameshuang337@gmail.com>
2022-02-09 15:29:13 -0800

    use "only" instead of "except" for category resources

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :destroy, :show]
+    resources :categories, only: [:index, :new, :create]
+    # resources :categories, except: [:edit, :update, :destroy, :show]
   end
 
   get '/about', to: 'about#index'
